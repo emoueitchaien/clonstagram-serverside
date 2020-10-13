e37ec94ff87d0bcd4f94511415b6a2aa95895fdf
emoueitchaien <manojpradhan628@gmail.com>
2020-10-13 23:46:36 +0545

    update package.json and key used

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const cors = require("cors");
 //MONGODB CONNECTION
 //vw2s8G1nB1L954uk
 
-const mongoose = require('mongoose')
-const {mongoURI} = require('./key') //receives URI FROM FILE key.js
+const mongoose = require("mongoose");
+const { mongoURI } = require("./key"); //receives URI FROM FILE key.js
 
-require('dotenv').config() //dotenv module access .env file
+require("dotenv").config(); //dotenv module access .env file
 const uri = process.env.mongoURI; //receives URI from file .env
 
-mongoose.connect(uri, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
+mongoose.connect(mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
-mongoose.connection.on('connected',()=>{
-    console.log('Mongodb connected successfully!')
-})
-  
-mongoose.connection.off('error',()=>{
-    console.log('Mongodb connection failed!')
-})
+mongoose.connection.on("connected", () => {
+  console.log("Mongodb connected successfully!");
+});
 
-require('./models/user')
-require('./models/post')
+mongoose.connection.off("error", () => {
+  console.log("Mongodb connection failed!");
+});
 
-app.use(cors())
-app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
+require("./models/user");
+require("./models/post");
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
 
 //MIDDLEWARES
 /*
@@ -42,13 +42,13 @@ const customMiddleware = (req,res,next) =>{
 app.use(customMiddleware)
 */
 
-//this is get request we basically do this in separate folder routes 
+//this is get request we basically do this in separate folder routes
 /*
 app.get('/',(req,res) => {
     res.send("Welcome to our Home Page!")
 })
 */
 
-app.listen(PORT,() =>{
-    console.log("Server started at",PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server started at", PORT);
+});
