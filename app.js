4b91d9a0e319729f9ebf45b2eb8eda848166b2e5
emoueitchaien <manojpradhan628@gmail.com>
2021-03-08 22:26:27 +0545

    setting up hosting to heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 //MONGODB CONNECTION
 //vw2s8G1nB1L954uk
